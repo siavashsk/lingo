3c0c16e264cbb645a5216a9dff75a592b63c88e8
siavash <siavash.khani200@gmail.com>
2024-03-24 22:15:51 +0330

    feat(courses):finished implementing initial routes

---
+++ scripts/seed.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import * as schema from "../db/schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+// @ts-ignore
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+    
+}
